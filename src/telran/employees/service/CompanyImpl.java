c90f9e9dc350aa94b825b6c47ab11aebbcfa4a3c
AlexKovalchuk <kovalchuk111@gmail.com>
2023-08-14 20:31:08 +0300

    HW #36 from classwork

--- src/telran/employees/service/CompanyImpl.java
+++ src/telran/employees/service/CompanyImpl.java
@@ -1,102 +1,94 @@
 package telran.employees.service;
 
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.*;
-
+import java.util.List;
 import java.util.stream.Collectors;
 
 import telran.employees.dto.DepartmentSalary;
 import telran.employees.dto.Employee;
 import telran.employees.dto.SalaryDistribution;
 
+import java.time.LocalDate;
+import java.util.*;
+
 public class CompanyImpl implements Company {
-	
-	private static final LocalDate CURRENT_DATE = LocalDate.now();
 	LinkedHashMap<Long, Employee> employees = new LinkedHashMap<>(); 
 	TreeMap<Integer, Collection<Employee>> employeesSalary = new TreeMap<>();
+	TreeMap<LocalDate, Collection<Employee>> employeesAge = new TreeMap<>();
 	HashMap<String, Collection<Employee>> employeesDepartment = new HashMap<>();
-	TreeMap<Integer, Collection<Employee>> employeesAge = new TreeMap<>();
 	
 	@Override
 	public boolean addEmployee(Employee empl) {
 		boolean res = false;
-		Employee emplRes = employees.putIfAbsent(empl.id(), empl); 
-		if(emplRes == null) {
+		Employee emplRes = employees.putIfAbsent(empl.id(), empl);
+		if (emplRes == null) {
 			res = true;
 			addEmployeeSalary(empl);
-			addEmployeeDepartment(empl);
 			addEmployeeAge(empl);
+			addEmployeeDepartment(empl);
 		}
 		return res;
 	}
+	
+	private <T> void addToIndex(Employee empl, T key, Map<T, Collection<Employee>> map) {
+		map.computeIfAbsent(key, k -> new HashSet<>()).add(empl);
+	}
 
+	private <T> void removeFromIndex(Employee empl, T key, Map<T, Collection<Employee>> map) {
+		Collection<Employee> employeesCol = map.get(key);
+		employeesCol.remove(empl);
+		if(employeesCol.isEmpty()) {
+			map.remove(key);
+		}
+	}
+	
+	private void addEmployeeSalary(Employee empl) {
+		addToIndex(empl, empl.salary(), employeesSalary);
+ 	}
+	
+	
 	private void addEmployeeAge(Employee empl) {
-		int age = getAge(empl.birthDate());
-		employeesAge.computeIfAbsent(age, k -> new HashSet<>())
-		.add(empl);
+		addToIndex(empl, empl.birthDate(), employeesAge);
 	}
 
 	private void addEmployeeDepartment(Employee empl) {
-		
-		String department = empl.department();
-		employeesDepartment.computeIfAbsent(department, k -> new HashSet<>())
-		.add(empl);
+		addToIndex(empl, empl.department(), employeesDepartment);
 	}
-
-	private void addEmployeeSalary(Employee empl) {
-		int salary = empl.salary();
-		employeesSalary.computeIfAbsent(salary, k -> new HashSet<>())
-		.add(empl);
- 	}
 
 	@Override
 	public Employee removeEmployee(long id) {
 		Employee res = employees.remove(id);
-		if(res != null) {
+		if (res != null) {
 			removeEmployeeSalary(res);
-			removeEmployeeDepartment(res);
 			removeEmployeeAge(res);
+			removeEmployeeDepartment(res);
 		}
 		return res;
 	}
  
+	private void removeEmployeeSalary(Employee empl) {
+		int salary = empl.salary();
+		removeFromIndex(empl, salary, employeesSalary);
+		
+	}
+	
+	
 	private void removeEmployeeAge(Employee empl) {
-		int age = getAge(empl.birthDate());
-		Collection<Employee> employeesCol = employeesAge.get(age);
-		employeesCol.remove(empl);
-		if(employeesCol.isEmpty()) {
-			employeesAge.remove(age);
-		}
+		removeFromIndex(empl, empl.birthDate(), employeesAge);
+		
 	}
 
 	private void removeEmployeeDepartment(Employee empl) {
-		String department = empl.department();
-		Collection<Employee> employeesCol = employeesDepartment.get(department);
-		employeesCol.remove(empl);
-		if(employeesCol.isEmpty()) {
-			employeesDepartment.remove(department);
-		}
-	}
-
-	private void removeEmployeeSalary(Employee empl) {
-		int salary = empl.salary();
-		Collection<Employee> employeesCol = employeesSalary.get(salary);
-		employeesCol.remove(empl);
-		if(employeesCol.isEmpty()) {
-			employeesSalary.remove(salary);
-		}
+		removeFromIndex(empl, empl.department(), employeesDepartment);
+	
 	}
 
 	@Override
 	public Employee getEmployee(long id) {
-		
 		return employees.get(id);
 	}
 
 	@Override
 	public List<Employee> getEmployees() {
-		
 		return new ArrayList<>(employees.values());
 	}
 
@@ -126,17 +118,12 @@ public class CompanyImpl implements Company {
 
 	@Override
 	public List<Employee> getEmployeesByDepartment(String department) {
-		if (employeesDepartment.get(department) == null) {
-			throw new IllegalArgumentException("department is not exist");
+		Collection<Employee> employeesCol = employeesDepartment.get(department);
+		ArrayList<Employee> res = new ArrayList<>();
+		if(employeesCol != null) {
+			res.addAll(employeesCol);
 		}
-
-		
-		
-			return employeesDepartment.get(department)
-				.stream().sorted((empl1, empl2) -> Integer.compare(empl1.salary(), empl2.salary()))
-				.toList();
-		 	
-		
+		return res;
 	}
 
 	@Override
@@ -150,28 +137,35 @@ public class CompanyImpl implements Company {
 
 	@Override
 	public List<Employee> getEmployeesByAge(int ageFrom, int ageTo) {
-		if(ageFrom > ageTo || ageFrom < 18 || ageTo > 100) {
-			throw new IllegalArgumentException("Wrong age");
-		}
-		return employeesAge.subMap(ageFrom, true,  ageTo, true)
-				.values().stream()
-				.flatMap(col -> col.stream().sorted((empl1, empl2) -> 
-				Integer.compare(empl1.salary(), empl2.salary()))).toList();
+		LocalDate dateTo = LocalDate.now().minusYears(ageFrom);
+		LocalDate dateFrom = LocalDate.now().minusYears(ageTo);
+		return employeesAge.subMap(dateFrom, true, dateTo, true).values()
+				.stream().flatMap(col -> col.stream())
+				.sorted((empl1, empl2) -> Long.compare(empl1.id(), empl2.id()))
+						.toList();
 	}
 
 	@Override
 	public Employee updateSalary(long id, int newSalary) {
-		// TODO Auto-generated method stub
-		return null;
+		Employee empl  = removeEmployee(id);
+		if(empl != null) {
+			Employee newEmployee = new Employee(id, empl.name(), 
+					empl.department(), newSalary, empl.birthDate());
+			addEmployee(newEmployee);
+		}
+		return empl;
 	}
 
 	@Override
 	public Employee updateDepartment(long id, String newDepartment) {
-		// TODO Auto-generated method stub
-		return null;
+		Employee empl = removeEmployee(id);
+		if(empl != null) {
+			Employee newEmployee = new Employee(id, empl.name(), newDepartment, 
+					empl.salary(), empl.birthDate());
+			addEmployee(newEmployee);
+		}
+		return empl;
 	}
 	
-	private int getAge(LocalDate birthDate) {
-		return Period.between(birthDate, CURRENT_DATE).getYears();
-	}
+	
 }
--- src/telran/employees/test/CompanyTest.java
+++ src/telran/employees/test/CompanyTest.java
@@ -3,11 +3,12 @@ package telran.employees.test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
-import java.time.MonthDay;
-import java.time.Period;
-import java.time.Year;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -21,10 +22,10 @@ class CompanyTest {
 	private static final long ID1 = 123;
 	private static final String DEP1 = "dep1";
 	private static final int SALARY1 = 10000;
-	private static final int YEAR1 = 2000; 
+	private static final int YEAR1 = 2000;
 	private static final LocalDate DATE1 = LocalDate.ofYearDay(YEAR1, 100);
 	private static final long ID2 = 124;
-	private static final long ID3 = 125; //125
+	private static final long ID3 = 125;
 	private static final long ID4 = 126;
 	private static final long ID5 = 127;
 	private static final String DEP2 = "dep2";
@@ -32,20 +33,19 @@ class CompanyTest {
 	private static final int SALARY2 = 5000;
 	private static final int SALARY3 = 15000;
 	private static final int YEAR2 = 1990;
-	private static final int YEAR3 = 2003;
 	private static final LocalDate DATE2 = LocalDate.ofYearDay(YEAR2, 100);
+	private static final int YEAR3 = 2003;
 	private static final LocalDate DATE3 = LocalDate.ofYearDay(YEAR3, 100);
 	private static final long ID_NOT_EXIST = 10000000;
 	private static final String TEST_DATA = "test.data";
-	private static final int INTERVAL = 5000;
 	Employee empl1 = new Employee(ID1, "name", DEP1, SALARY1, DATE1);
 	Employee empl2 = new Employee(ID2, "name", DEP2, SALARY2, DATE2);
-	Employee empl3 = new Employee(ID3, "name", DEP1, SALARY1, DATE1); 
+	Employee empl3 = new Employee(ID3, "name", DEP1, SALARY1, DATE1);
 	Employee empl4 = new Employee(ID4, "name", DEP2, SALARY2, DATE2);
 	Employee empl5 = new Employee(ID5, "name", DEP3, SALARY3, DATE3);
-	Employee[] employees = {empl1, empl2, empl3, empl4, empl5}; 
+	Employee[] employees = {empl1, empl2, empl3, empl4, empl5};
 	Company company;
-	
+
 	@BeforeEach
 	void setUp() throws Exception {
 		company = new CompanyImpl();
@@ -64,9 +64,10 @@ class CompanyTest {
 	void testRemoveEmployee() {
 		assertNull(company.removeEmployee(ID_NOT_EXIST));
 		assertEquals(empl1, company.removeEmployee(ID1));
-		Employee[] expected = {empl2, empl3, empl4, empl5}; 
+		Employee[] expected = {empl2, empl3, empl4, empl5};
 		assertArrayEquals(expected, company.getEmployees()
 				.toArray(Employee[]::new));
+		
 	}
 
 	@Test
@@ -83,67 +84,32 @@ class CompanyTest {
 
 	@Test
 	void testGetDepartmentSalaryDistribution() {
-		DepartmentSalary[] expected = {
-				new DepartmentSalary(DEP2, SALARY2),
-				new DepartmentSalary(DEP1, SALARY1),
-				new DepartmentSalary(DEP3, SALARY3)
-				};
-		DepartmentSalary[] actual = company.getDepartmentSalaryDistribution()
-				.stream().sorted((ds1, ds2) -> Double.compare(ds1.salary(), ds2.salary()))
-				.toArray(DepartmentSalary[]::new);
+		DepartmentSalary [] expected = {
+			new DepartmentSalary(DEP2, SALARY2),
+			new DepartmentSalary(DEP1, SALARY1),
+			new DepartmentSalary(DEP3, SALARY3)
+		};
+		DepartmentSalary [] actual = company.getDepartmentSalaryDistribution()
+				.stream().sorted((ds1, ds2) -> Double.compare(ds1.salary(), ds2.salary())).
+				toArray(DepartmentSalary[]::new);
 		assertArrayEquals(expected, actual);
 	}
 
 	@Test
 	void testGetSalaryDistribution() {
-		int interval = 5000; 
+		int interval = 5000;
 		SalaryDistribution[] expected = {
 				new SalaryDistribution(SALARY2, SALARY2 + interval - 1, 2),
 				new SalaryDistribution(SALARY1, SALARY1 + interval - 1, 3),
-				new SalaryDistribution(SALARY3, SALARY3 + interval - 1, 1)
+				new SalaryDistribution(SALARY3, SALARY3 + interval - 1, 1),
 		};
-		company.addEmployee(new Employee(ID_NOT_EXIST, "name", DEP2, 13000, DATE1));
-		SalaryDistribution[] actual = company.getSalaryDistribution(interval)
+		company.addEmployee(new Employee(ID_NOT_EXIST, DEP2, DEP2, 13000,  DATE1));
+		SalaryDistribution[] actual =
+				company.getSalaryDistribution(interval)
 				.toArray(SalaryDistribution[]::new);
 		assertArrayEquals(expected, actual);
 	}
-	
-	@Test
-	void testGetEmployeesBySalary() {      //sal2 - sal1:  5000 - 10000: empl2, empl4, empl1, empl3
-		Employee[] expected = {empl2, empl4, empl1, empl3};
-	
-		Employee[] actual = (Employee[]) company.getEmployeesBySalary(SALARY2, SALARY1)
-				.toArray(Employee[]::new);
-		assertArrayEquals(expected, actual);
-		assertTrue(company.getEmployeesBySalary(4000, 4999).isEmpty());
-		assertEquals(5, company.getEmployeesBySalary(SALARY2, SALARY3).size());
-	}
-	
-	@Test
-	void testGetEmployeesByDepartment() {
-		Employee[] expected = {empl3, empl1, new Employee(ID_NOT_EXIST, "name", DEP1, SALARY3, DATE1)};
-		company.addEmployee(new Employee(ID_NOT_EXIST, "name", DEP1, SALARY3, DATE1));
-		assertArrayEquals(expected, company.getEmployeesByDepartment(DEP1)
-				.toArray(Employee[]::new));
-		
-		assertThrows(IllegalArgumentException.class, 
-				() -> company.getEmployeesByDepartment("no dep"));
-	}
-	
-	@Test
-	void testGetEmployeesByAge() {
 
-		//DATE1 = 23. DATE2 = 33, DATE3 = 20
-		System.out.println(company.getEmployeesByAge(23, 33));
-		Employee[] expected = {empl3, empl1, empl4, empl2};
-		assertArrayEquals(expected, company.getEmployeesByAge(23, 33).toArray(Employee[]::new));
-		Employee[] expected2 = {empl5};
-		assertArrayEquals(expected2, company.getEmployeesByAge(20, 20).toArray(Employee[]::new));
-		assertTrue(company.getEmployeesByAge(40, 50).isEmpty());
-		assertThrows(IllegalArgumentException.class, 
-				() -> company.getEmployeesByAge(25, 20));
-	}
-	
 	@Test
 	@Order(2)
 	void testRestore() {
@@ -151,12 +117,50 @@ class CompanyTest {
 		newCompany.restore(TEST_DATA);
 		assertArrayEquals(employees, newCompany.getEmployees()
 				.toArray(Employee[]::new));
+		
 	}
 
 	@Test
 	@Order(1)
 	void testSave() {
 		company.save(TEST_DATA);
+	}
+	private void runGetByDepartmentTest(String department, Employee[] expected) {
+		List<Employee> employees = company.getEmployeesByDepartment(department);
+		employees.sort((e1, e2) -> Long.compare(e1.id(), e2.id()));
+		assertArrayEquals(expected, employees.toArray(Employee[]::new));
+	}
+	private void runGetBySalaryTest(int salaryFrom, int salaryTo,
+			Employee[]expected) {
+		List<Employee> employees = new ArrayList<>
+		(company.getEmployeesBySalary(salaryFrom, salaryTo));
+				
+		
+		employees.sort((e1, e2) -> Long.compare(e1.id(), e2.id()));
+		assertArrayEquals(expected, employees.toArray(Employee[]::new));
+	}
+	@Test
+	void getEmployeesByDepartmentTest() {
+		runGetByDepartmentTest("XXX", new Employee[0]);
+		runGetByDepartmentTest(DEP1, new Employee[] {empl1, empl3});
+	}
+	@Test
+	void updateDepartmentTest() {
+		company.updateDepartment(ID5, DEP1);
+		runGetByDepartmentTest(DEP1, new Employee[] {empl1, empl3, empl5});
+		runGetByDepartmentTest(DEP3, new Employee[0]);
+	}
+	@Test
+	void getEmployeesBySalaryTest() {
+		runGetBySalaryTest(SALARY2, SALARY3, employees);
+		runGetBySalaryTest(SALARY3 + 1, 100000000, new Employee[0]);
+		runGetBySalaryTest(SALARY2, SALARY1, new Employee[] {empl1, empl2, empl3, empl4});
+	}
+	@Test
+	void updateSalaryTest() {
+		company.updateSalary(ID5, SALARY1);
+		runGetBySalaryTest(SALARY3, SALARY3 + 1, new Employee[0]);
+		runGetBySalaryTest(SALARY2, SALARY1,employees);
 	}
 
 }
